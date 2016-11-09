6381c102b2a9ed07df2c1d9743e0e7281b2b4e6c
Albert <anitu314@gmail.com>
2016-11-08 23:23:05 -0500

    Create theGameStyle.js & create function for game

---
+++ JavaScript/theGameScript.js
@@ -0,0 +1,7 @@
+function loseGame() {
+  alert("Congratulations, you chose to lose the game! As a result, you lost the game!");
+}
+
+function winGame() {
+  alert("Sorry, but you can't win the game...\nOn the other hand, what you just did was... LOSE THE GAME!");
+}
