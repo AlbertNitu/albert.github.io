6a55089a29c08e21cfb7b7c69c4dfab36ed58601
Albert <anitu314@gmail.com>
2016-11-13 08:21:33 -0500

    Testing local storage on snakeGameScript.js

--- JavaScript/snakeGameScript.js
+++ JavaScript/snakeGameScript.js
@@ -17,6 +17,7 @@ $(document).ready(function(){
 	
 	function init()
 	{
+		alert(localStorage.getItem("High Score"));
 		d = "right"; //default direction
 		create_snake();
 		create_food(); //Now we can see the food particle
